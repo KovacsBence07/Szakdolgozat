7030a50325c4c5cad6ed085d2f0bdde9a3e8726c
Tomii1024 <tomcsi009@gmail.com>
2022-04-27 14:31:19 +0200

    Navbar push

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,73 @@
 <template>
-  <div class="app">
-    <nav class="header-1">
-      <a href="" class="logo">Vizsgaremek</a>
+  <div>
+    <div class="wrapper">
+      <div class="menu">
+        <a href="#"> <i class="fa fa-home"> </i> </a>
 
-      <form action="" class="search-box-container">
-        <input type="search" id="search-box" placeholder="Search here..." />
-        <label for="search-box" class="fas fa-search"></label>
-      </form>
-    </nav>
-    <nav class="header-2">
-      <div id="menu-bar" class="fas fa-bars"></div>
-
-      <nav class="navbar">
+        <!-- <div class="dropdown">
+                <a href="#"> Categories </a>
+                <div class="submenu">
+                    <div class="submenu1">
+                        <a href="#"> Decor </a>
+                        <div class="decorsubmenu">
+                            <a href="#"> Wall Decor </a>
+                            <a href="#"> Home Decor </a>
+                        </div>
+                    </div>
+                    <div class="submenu2">
+                        <a href="#"> Accesorries </a>
+                        <div class="accessoriessubmenu">
+                            <a href="#"> Earring </a>     
+                            <a href="#"> Bracelet </a>
+                            <a href="#"> Neck Piece </a>
+                            <a href="#"> Anklets </a>
+                            <a href="#"> Rings </a>
+                        </div>
+                    </div>
+                    <a href="#"> Bags </a>
+                    <a href="#"> Baskets/Boxes </a>
+                    <a href="#"> Notebooks </a>
+                    <a href="#"> Gifts </a>
+                </div>
+            </div> -->
+       
         <router-link to="/home" class="nav-link">Home</router-link>
         <span v-if="isLoggedIn">
-        <router-link to="/products" class="nav-link">Admin products</router-link>
+          <router-link to="/products" class="nav-link"
+            >Admin products</router-link>
         </span>
         <span v-if="isLoggedIn">
-        <router-link to="/add" class="nav-link">Add Product</router-link>
+          <router-link to="/add" class="nav-link">Add Product</router-link>
         </span>
-        <router-link to="/userProducts" class="nav-link">User products</router-link>
-        
+        <router-link to="/userProducts" class="nav-link"
+          >User products</router-link>
         <router-link to="/about" class="nav-link">About</router-link>
-        
-      </nav>
-   
-      <span v-if="isLoggedIn" class="buttons">
-        <button class="btn btn-light btn-lg ms-1 mr-2" @click="Logout">Logout</button>
-      </span>
-      <span v-else>
-        
-          <button class="btn btn-light ms-3 mr-2">
-            <router-link to="/login" class="nav-link">Login</router-link>
-          </button>
 
-          <button class="btn btn-light ms-3 mr-2">
-            <router-link to="/register" class="nav-link">Register</router-link>
-          </button>
-        
-      </span>
-     
-      <div class="icons">
-        <a href="#" class="fas fa-shopping-cart"></a>
-        
+         <div class="menu2">
+          <span v-if="isLoggedIn">
+            <a href="#" @click="Logout">Log out</a>
+          </span>
+          <span v-else>
+            <router-link to="/login" class="fa fa-sign-in"> Log in</router-link>
+
+            <router-link to="/register" class="fa fa-user"> Register</router-link>
+              <a href="#" id="cart">
+              <i class="fa fa-shopping-cart"></i> View Cart
+              </a>
+
+          </span>
+          
+        </div>
       </div>
+      <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
       <link
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       />
-    </nav>
+      
+    </div>
     <div class="container mt-3"></div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <script>
@@ -101,190 +118,87 @@ export default {
 };
 </script>
 <style>
-
-.buttons{
-  position: relative;
-
-}
 * {
-  font-family: "Nunito", sans-serif;
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
-  border: none;
-  outline: none;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 1em;
+}
+.menu2{
+  float: right;
+  display: flex;
+  margin-left: 1300px;
+}
+.menu {
+  background-color: #36486b;
+  display: flex;
+}
+.menu a {
+  display: inline-block;
+  padding: 15px 10px 15px 10px;
   text-decoration: none;
+  color: white;
+  border: 1px solid #36486b;
 }
-
-:root {
-  --green: #008080;
-  --black: #2c2c54;
+.menu a:hover {
+  background-color: white;
+  color: #36486b;
 }
-
-::selection {
-  background: var(--green);
-  color: #fff;
-}
-
-html {
-  font-size: 60%;
-  overflow-x: hidden;
-  scroll-padding-top: 5rem;
-  scroll-behavior: smooth;
-}
-
-.header-1 {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 2rem 1%;
-  background-color: #eee;
-}
-
-.header-1 .logo {
-  font-size: 3rem;
-  color: var(--black);
-  font-weight: bolder;
-}
-
-.header-1 .logo i {
-  color: var(--green);
-  padding: 0.5rem;
-}
-
-.header-1 .search-box-container {
-  display: flex;
-  height: 5rem;
-}
-
-.header-1 .search-box-container #search-box {
-  height: 100%;
-  width: 100%;
-  padding: 1rem;
-  border: 0.1rem solid rgba(0, 0, 0, 0.3);
-  color: #333;
-  font-size: 2rem;
-  text-transform: none;
-}
-
-.header-1 .search-box-container label {
-  width: 8rem;
-  height: 100%;
-  background-color: var(--green);
-  font-size: 2.5rem;
-  line-height: 5rem;
-  color: #fff;
-  cursor: pointer;
-  text-align: center;
-}
-
-.header-1 .search-box-container label:hover {
-  background-color: var(--black);
-}
-
-.header-2 {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem 9%;
-  box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 10%);
-  background-color: #fff;
+.dropdown {
   position: relative;
+  display: inline-block;
 }
-
-.header-2.active {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-
-.header-2 .navbar a {
-  padding: 0.5rem 1.5rem;
-  font-size: 2rem;
-  color: var(--black);
-  border-radius: 0.5rem;
-}
-
-.header-2 .navbar a:hover {
-  color: #fff;
-  background-color: var(--green);
-}
-
-.header-2 .icons a {
-  font-size: 2.5rem;
-  color: var(--black);
-  padding-left: 1rem;
-}
-
-.header-2 .icons a:hover {
-  color: var(--green);
-}
-
-#menu-bar {
-  font-size: 3rem;
-  border-radius: 0.5rem;
-  border: 0.1rem solid var(--black);
-  padding: 0.8rem 1.5rem;
-  cursor: pointer;
-  color: var(--black);
+.submenu {
   display: none;
+  position: absolute;
+  background-color: #36486b;
+  color: white;
 }
-
-/* ----------------- media query start -----------------  */
-
-@media (max-width: 1200px) {
-  html {
-    font-size: 55%;
-  }
+.submenu a {
+  display: block;
+  border: 1px solid #36486b;
 }
-
-@media (max-width: 991px) {
-  .header-1,
-  .header-2 {
-    padding: 2rem;
-  }
+.dropdown:hover .submenu {
+  display: block;
 }
-
-@media (max-width: 768px) {
-  #menu-bar {
-    display: initial;
-  }
-  .header-2 .navbar {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    right: 0;
-    background: var(--green);
-    padding: 1rem 2rem;
-    display: none;
-  }
-  .header-2 .navbar.active {
-    display: initial;
-  }
-  .header-2 .navbar a {
-    font-size: 2.5rem;
-    display: block;
-    background: #fff;
-    text-align: center;
-    padding: 1rem;
-    margin: 1.5rem 0;
-  }
+/*Dropdown decor submenu*/
+.submenu1 {
+  position: relative;
+  display: inline-block;
 }
-
-@media (max-width: 450px) {
-  html {
-    font-size: 50%;
-  }
-  .header-1 {
-    flex-flow: column;
-  }
-  .header-1 .search-box-container {
-    width: 100%;
-    margin-top: 2rem;
-  }
-  .header-1 .logo {
-    font-size: 4rem;
-  }
+.decorsubmenu {
+  display: none;
+  position: absolute;
+  background-color: #36486b;
+  color: white;
+  margin-left: 116px;
+  margin-top: -50px;
 }
+.decorsubmenu a {
+  display: block;
+}
+.submenu1:hover .decorsubmenu {
+  display: block;
+}
+/*Dropdown Accessories submenu*/
+.submenu2 {
+  position: relative;
+  display: inline-block;
+}
+.accessoriessubmenu {
+  display: none;
+  position: absolute;
+  background-color: #36486b;
+  color: white;
+  margin-left: 116px;
+  margin-top: -50px;
+}
+.accessoriessubmenu a {
+  display: block;
+}
+.submenu2:hover .accessoriessubmenu {
+  display: block;
+}
+/*Right NavBar*/
+
 </style>
